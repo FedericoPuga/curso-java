0639efe4a56efc216ed3146eaa169d679eff4342
FedericoPuga <fede.puga.18@gmail.com>
2023-01-08 19:40:00 -0300

    agrego clase 4

---
+++ clase04/script.js
@@ -0,0 +1,28 @@
+function saludar(clase , curso) {
+    console.log(`Hola clase 4: ${clase}. Este es el curso de ${curso} `);
+}
+let valor = saludar("funciones!","java scrip")
+
+
+function sumar(valor1 , valor2) {
+    let total = valor1 + valor2
+    console.log(`El total es ${total}`);
+}
+sumar(2,2)
+
+
+
+//Declaración de variable para guardar el resultado de la suma
+let resultado = 0;
+//Función que suma dos números y asigna a resultado
+function sumar(primerNumero, segundoNumero) {
+    resultado = primerNumero + segundoNumero
+}
+//Función que muestra resultado por consola
+function mostrar(mensaje) {
+    console.log(mensaje)
+}
+//Llamamos primero a sumar y luego a mostrar
+sumar(6, 3);            
+mostrar(resultado); 
+
